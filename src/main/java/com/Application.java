90ab96c5eda177d50c0f1b7028a45524da273090
wangjinbian <kingslay@icloud.com>
2015-12-01 23:49:40 +0800

    no message

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,8 +1,13 @@
 package com;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.SocketUtils;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,5 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public Integer port() {
+        return 8280;
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port());
+        return connector;
     }
 }
