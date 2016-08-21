7ad7dce3944591bc2dd93152283c86b98606960a
wangjinbian <kingslay@icloud.com>
2016-08-21 22:31:01 +0800

    no message

--- server/src/main/java/com/Application.java
+++ server/src/main/java/com/Application.java
@@ -3,36 +3,36 @@ package com;
 import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.util.SocketUtils;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by king on 15/11/25.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-
     @Bean
     public Integer port() {
-        return 8280;
+        return  8280;
+//        return SocketUtils.findAvailableTcpPort();
     }
-
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
         return tomcat;
     }
 
-    private Connector createSslConnector() {
+    private Connector createStandardConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setPort(port());
         return connector;
